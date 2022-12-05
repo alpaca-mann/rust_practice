2808ef67e00cc0b43bd0a534ccb2f55029465526
Alpaca man <johnerhover@gmail.com>
2022-12-06 02:11:48 +0900

    part 2.2 mismatched type error

--- Chapter_2/gussing_game_in_rust/game_part_2/src/main.rs
+++ Chapter_2/gussing_game_in_rust/game_part_2/src/main.rs
@@ -4,7 +4,9 @@ use rand::Rng;
 fn main() {
     println!("Guess the number");
 
+    /*Added - Part 2 Start */
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    /*Added - Part 2 End*/
 
     println!("The secret number is: {secret_number}");
 
@@ -17,6 +19,14 @@ fn main() {
     .expect("Failed to read line");
 
     println!("you guessed: {guess}");
+
+    /*Added - Part 2.2 Start */
+    match guess.cmp(&secret_number){ //The compiler error: saying "guess" and "secret_number" is different types.
+        Ordering::Less=>println!("Too smol"),
+        Ordering::Greater=>println!("Too bic"),
+        Ordering::Equal=>println!("yay!"),
+    }
+    /*Added - Part 2.2 End*/
 }
 
 /*・Things done・*/
@@ -26,6 +36,9 @@ import rand dependency in cargo.toml
 after importing test build. (cargo build)
 
 #note: if program hangs with "waiting for ....", use "cargo clean" to reset the project. code won't be affected.
+
+2.Comparing the Guess to the Secret Number.
+
 */
 
 /*・Study notes・*/
