0ee41f7ed5e01cdc7b21a5340d7bb17515015856
Alpaca man <johnerhover@gmail.com>
2022-12-06 00:59:48 +0900

    guessing game part2

---
+++ Chapter_2/gussing_game_in_rust/game_part_2/README.md
@@ -0,0 +1,3 @@
+<h1>Rust Book Guessing game part2</h2>
+
+<p>From <a href="https://doc.rust-lang.org/stable/book/ch02-00-guessing-game-tutorial.html#generating-a-secret-number">Generating a Secreat Number</a<</p>
---
+++ Chapter_2/gussing_game_in_rust/game_part_2/target/debug/deps/game_part_2-3e3b08f96d467b0d.d
@@ -0,0 +1,5 @@
+c:\Users\johne\rust_practice\Chapter_2\gussing_game_in_rust\game_part_2\target\debug\deps\game_part_2-3e3b08f96d467b0d.rmeta: src\main.rs
+
+c:\Users\johne\rust_practice\Chapter_2\gussing_game_in_rust\game_part_2\target\debug\deps\game_part_2-3e3b08f96d467b0d.d: src\main.rs
+
+src\main.rs:
