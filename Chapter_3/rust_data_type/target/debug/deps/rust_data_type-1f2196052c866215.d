4f42d8375ddb3a35111d341fae5b11e4eeba20ca
Alpaca man <johnerhover@gmail.com>
2022-12-11 23:49:33 +0900

    cleaning done

--- Chapter_3/rust_data_type/target/debug/deps/rust_data_type-1f2196052c866215.d
+++
@@ -1,5 +0,0 @@
-c:\Users\johne\rust_practice\Chapter_3\rust_data_type\target\debug\deps\rust_data_type-1f2196052c866215.rmeta: src\main.rs
-
-c:\Users\johne\rust_practice\Chapter_3\rust_data_type\target\debug\deps\rust_data_type-1f2196052c866215.d: src\main.rs
-
-src\main.rs:
--- Chapter_3/rust_data_type/target/debug/deps/rust_data_type-8870e74eceec2516.d
+++
@@ -1,5 +0,0 @@
-c:\Users\johne\rust_practice\Chapter_3\rust_data_type\target\debug\deps\rust_data_type-8870e74eceec2516.rmeta: src\main.rs
-
-c:\Users\johne\rust_practice\Chapter_3\rust_data_type\target\debug\deps\rust_data_type-8870e74eceec2516.d: src\main.rs
-
-src\main.rs:
